6d97849a20b3674292e2a059446101417ae6ffaf
seongtak <nt10930@nhn.com>
2015-12-08 00:43:17 +0900

    [MapMo] 최초 커밋

---
+++ src/main/java/com/taky/mapmo/MapMoApplication.java
@@ -0,0 +1,12 @@
+package com.taky.mapmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapMoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapMoApplication.class, args);
+    }
+}
