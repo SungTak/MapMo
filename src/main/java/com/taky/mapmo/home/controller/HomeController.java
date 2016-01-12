714e915290dd4089651711b93d30135a336084c4
seongtak <nt10930@nhn.com>
2016-01-12 19:17:04 +0900

    [MapMo] logback 설정 및 로그 연동

--- src/main/java/com/taky/mapmo/home/controller/HomeController.java
+++ src/main/java/com/taky/mapmo/home/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.taky.mapmo.home.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,11 +23,15 @@ import com.taky.mapmo.home.service.HomeService;
  */
 @Controller
 public class HomeController {
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
 	@Autowired
 	private HomeService homeService;
 	
 	@RequestMapping(value = "/")
 	public ModelAndView findHome() {
+		logger.info("### 홈 화면에 접근하였습니다~ 환영합니다. {}", "ㅎㅎㅎ");
+		
 		ModelAndView model = new ModelAndView("main");
 		model.addObject("say", "hello~~!");
 		return model;
