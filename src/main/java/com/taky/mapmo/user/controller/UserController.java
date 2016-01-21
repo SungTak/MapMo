d1ea0c849d397bafc75d8523b151319f253554d6
seongtak <nt10930@nhn.com>
2016-01-21 18:34:29 +0900

    [MapMo] 가입 기능

--- src/main/java/com/taky/mapmo/user/controller/UserController.java
+++ src/main/java/com/taky/mapmo/user/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.taky.mapmo.user.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +16,16 @@ import com.taky.mapmo.user.service.UserService;
  */
 @Controller
 public class UserController {
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 	
 	@Autowired
 	private UserService userService;
+	
+	@RequestMapping(value = "/join")
+	public String join() throws Exception {
+		logger.info("### 신규 가입 요청");
+		return "user/join";
+	}
 	
 	@RequestMapping(value = "/signin")
 	public ModelAndView registUser() throws Exception {
